3e60f013322d84addb5b079dd25dd409f5788b07
Chandaluri <S530742@nwmissouri.edu>
2018-07-19 17:00:00 -0500

    Getting password

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,32 +1,40 @@
 <template>
 <div>
    <p>Add My student details</p>
-   <input type="text" hint="Enter Name" v-model="sname">
-   <button class="submit" @click="submit">Submit</button> 
+   <input type="text" hint="Enter mail id:" v-model="sname">
+   <button class="submit" @click="submit">Search</button>
+   <p>Password: {{ student_password }} </p>
 </div>
 </template>
 <script>
 import axios from "axios";
 export default {
-    data() {
-        return {
-            sname: ""
-        }
-    },
-    methods: {
-        submit(){
-var instance = axios.create({
-  baseURL: 'http://localhost:3000/user/details',
-  headers: {'x-auth': window.localStorage.getItem('token')}
-});
-instance.post(this.data).then((res) =>{
-    console.log(res)
-}).catch((err) => {
-    console.log(err);
-});
-;
-}
-
+  data() {
+    return {
+      sname: "",
+      student_password: ""
+    };
+  },
+  methods: {
+    submit() {
+      console.log(window.localStorage.getItem("token"));
+      axios({
+          method:'post',
+          url: 'http://localhost:3000/user/details',
+          headers: {
+          'x-auth': window.localStorage.getItem("token")
+         },
+        data: {
+            sname: this.sname,
+            }
+      }).then(res => {
+          this.student_password = res.data
+          console.log(res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
-}
+  }
+};
 </script>
